63442a723dcd9c1c55016afc94b771f42962d976
amrmuhammad <eng.amrmuhammad@gmail.com>
2016-07-10 15:17:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -77,5 +77,20 @@ Perceptron.prototype.constructor = Perceptron;
 
 var no_of_neurons = ;
 var myPerceptron = new Perceptron(2,3,1);
- 
-var myTrainer = new Trainer(myPerceptron); myTrainer.XOR(); // { error: 0.004998819355993572, iterations: 21871, time: 356 } myPerceptron.activate([0,0]); // 0.0268581547421616 myPerceptron.activate([1,0]); // 0.9829673642853368 myPerceptron.activate([0,1]); // 0.9831714267395621 myPerceptron.activate([1,1]); // 0.02128894618097928
+
+
+//////////////////////////////////////////
+// var myTrainer = new Trainer(myPerceptron); 
+// myTrainer.XOR(); 
+// { error: 0.004998819355993572, 
+// iterations: 21871, time: 356 } 
+// myPerceptron.activate([0,0]); 
+// 0.0268581547421616 
+// myPerceptron.activate([1,0]); 
+// 0.9829673642853368 
+// myPerceptron.activate([0,1]); 
+// 0.9831714267395621 
+// myPerceptron.activate([1,1]); 
+// 0.02128894618097928
+//////////////////////////////////////////
+
