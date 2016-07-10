57f95efaa54abbb7c7bd989194045dc1f4a0e710
amrmuhammad <eng.amrmuhammad@gmail.com>
2016-07-10 14:23:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+
+
+
+/////////////////////////////////////////
+
+// Neuron#1: element_id  // if zero, 
+             the element does not exist
+          
+// Neuron#2: parent_element_id
+// Neuron#3: html_element_type
+// Neuron#4: html_element_attribute_id
+// Neuron#5: html_element_attribute_type
+// .
+// .
+// .
+// .
+// Neuron#6: html_element_css_property_name
+// Neuron#7: html_element_css_property_value
+// .
+// .
+// .
+// .
+//////////////////////////////////////
+// we will assume for now that we can have
+// 200 html elements in the page
+// each element has an id, a type and a 
+// parent id
+// so that's 200*3 = 600 input neurons
+
+// we will assume that each element can have
+// 10 html attributes and 50 CSS properties
+
+// so that's 200*10*2 + 200*50*2= 
+// 4000 + 20,000 = 24, 000 input neurons
+
+
+
+
+
+
+
 /////////////////////////////////////////
 var synaptic = require('synaptic'); 
 
@@ -33,3 +74,8 @@ function Perceptron(input, hidden, output)
 // extend the prototype chain 
 Perceptron.prototype = new Network(); 
 Perceptron.prototype.constructor = Perceptron;
+
+var no_of_neurons = ;
+var myPerceptron = new Perceptron(2,3,1);
+ 
+var myTrainer = new Trainer(myPerceptron); myTrainer.XOR(); // { error: 0.004998819355993572, iterations: 21871, time: 356 } myPerceptron.activate([0,0]); // 0.0268581547421616 myPerceptron.activate([1,0]); // 0.9829673642853368 myPerceptron.activate([0,1]); // 0.9831714267395621 myPerceptron.activate([1,1]); // 0.02128894618097928
