e6f6b9784dcdcd7dc55b3d9549b34d73c507d258
amrmuhammad <eng.amrmuhammad@gmail.com>
2016-07-11 13:50:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -104,4 +104,24 @@ var myPerceptron = new Perceptron(
 // myPerceptron.activate([1,1]); 
 // 0.02128894618097928
 //////////////////////////////////////////
+/*********************
+// train the network 
+var learningRate = .3; 
+for (var i = 0; i < 20000; i++) 
+{ 
+  // 0,0 => 0 
+  myNetwork.activate([0,0]); 
+  myNetwork.propagate(learningRate, [0]); 
+  // 0,1 => 1 
+  myNetwork.activate([0,1]); 
+  myNetwork.propagate(learningRate, [1]); 
+  // 1,0 => 1 
+  myNetwork.activate([1,0]); 
+  myNetwork.propagate(learningRate, [1]); 
+  // 1,1 => 0 
+  myNetwork.activate([1,1]); 
+  myNetwork.propagate(learningRate, [0]); 
+}
+**************************/
+////////////////////////////////////
 
