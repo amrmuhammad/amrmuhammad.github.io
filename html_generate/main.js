9e134cb1ffbd41093de699486ec0574cf428f2d5
amrmuhammad <eng.amrmuhammad@gmail.com>
2016-09-10 22:05:04 +0200

    Update main.js

--- html_generate/main.js
+++ html_generate/main.js
@@ -199,7 +199,7 @@ $(document).ready(function() {
     <input id=\"user_feedback_text\" type=\"text\"></input> \
     <label for=\"user_action_text\"> User action </label> \
     <input id=\"user_action_text\" type=\"text\"></input> \
-    <button id=\"user_feedback_submit\" > </button>
+    <button id=\"user_feedback_submit\" > </button> \
   </div>";
 
   $("body").append(html_output);
