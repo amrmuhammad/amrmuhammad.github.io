b235fec1d21680fa10de2ec1d54ee11fb8ae4137
amrmuhammad <eng.amrmuhammad@gmail.com>
2016-12-24 23:03:51 +0200

    Update app.vue

--- math/js/vuejs/app.vue
+++ math/js/vuejs/app.vue
@@ -1,1 +1,20 @@
+<template> 
+  <div class="matrix">{{ elems }}</div> 
+</template> 
 
+
+<script>
+export default {
+  data () {
+    return {
+      elems: 'Hello world!'
+    }
+  }
+}
+</script> 
+
+<style>
+.matrix {
+  color: red;
+}
+</style>
