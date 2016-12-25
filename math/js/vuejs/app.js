89734841e0fe5efd22aa77eb9c0ac62b15c6f858
amrmuhammad <eng.amrmuhammad@gmail.com>
2016-12-25 13:11:05 +0200

    Update app.js

--- math/js/vuejs/app.js
+++ math/js/vuejs/app.js
@@ -1,16 +1,17 @@
 var Vue = require ('../lib/vuejs/vue.js')
 var VueApp = require ('./app.vue')
 
-Vue.component('matrix', {
-  props: ['rows'],
-  template: '<li>{{ rows }}</li>'
-})
+
 var app = new Vue({
   el: '#app',
+  
+  template: '<matrix> </matrix>',
+  
   data: {
-    rows: [
+    elems: [
       [1, 2, 3], 
       [4, 5, 6]
     ]
   }
+  
 })
