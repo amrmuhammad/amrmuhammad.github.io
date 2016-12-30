f5ed52df45735c997dbeb6f9fddb82f0be8c7d56
amrmuhammad <eng.amrmuhammad@gmail.com>
2016-12-30 20:36:50 +0200

    Update app.js

--- math/js/vuejs/app.js
+++ math/js/vuejs/app.js
@@ -1,7 +1,15 @@
 var Vue = require ('../lib/vuejs/vue.js')
 //var VueApp = require ('./app.vue')
 
-var matrix_template = ''
+var matrix_template = 
+    ' \
+    <template v-for="r in rows-count"> \
+      <div id="row"> \
+        <input v-for="c in cols-count" type="text"> \
+
+      </div> \
+    </template> \
+    '
 
 Vue.component ('matrix',  {
   template: matrix_template, 
