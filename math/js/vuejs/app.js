6ff5b630cd0581c567d91b8e60e92fed1e92b0f1
amrmuhammad <eng.amrmuhammad@gmail.com>
2016-12-31 00:58:17 +0200

    Update app.js

--- math/js/vuejs/app.js
+++ math/js/vuejs/app.js
@@ -6,7 +6,7 @@ var matrix_template = '\
     <div id="matrix" class="container">\
       <template v-for="r in 3">\
         <div id="row" class="row">\
-          <input v-for="c in 3" type="text" class="col-xs-4">\
+          <input v-for="c in 3" type="text" class="col-xs-2">\
         </div>\
       </template>\
     </div>\
