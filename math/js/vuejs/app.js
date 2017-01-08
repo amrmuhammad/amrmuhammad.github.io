1435c25cad37a49e252c8aa7cd1afd1562160289
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-01-09 01:35:06 +0200

    Update app.js

--- math/js/vuejs/app.js
+++ math/js/vuejs/app.js
@@ -25,10 +25,10 @@ var matrix_style_object = {
 var mso = matrix_style_object
 
 var matrix_template = '\
-    <div id="matrix" class="container" v-bind:style="mso.container">\
+    <div id="matrix" v-bind:style="mso.container">\
       <template v-for="r in 3">\
-        <div id="row" class="row" v-bind:style="mso.row">\
-          <div v-for="c in 3" class="col-xs-2" v-bind:style="mso.col">\
+        <div id="row" v-bind:style="mso.row">\
+          <div v-for="c in 3" v-bind:style="mso.col">\
             <input type="text" >\
           </div>\
         </div>\
