6a0c525d6180674ca6e0d66487100a8a29e14149
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-02-08 05:58:28 +0200

    Update main.js

--- css_in_action/js/main.js
+++ css_in_action/js/main.js
@@ -1,7 +1,10 @@
 var $ = require ('./lib/jquery/jquery.js')
 
 generate_example2_html() {
+  htmlString = '<div id=example2>'
   
+  htmlString += '</div>'
+  $('body').append(htmlString)
 }
 
 generate_html() {
