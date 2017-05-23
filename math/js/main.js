570fd55555bc61f0647586f67ba11440464d0cff
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-05-23 22:54:23 +0200

    Update main.js

--- math/js/main.js
+++ math/js/main.js
@@ -2,6 +2,19 @@
 
 var Vue = require('./lib/vuejs/v2_2_3/vue.js')
 
+
+/////////////////////
+Vue.component('matrix', {
+  template: '<div id="" > ' +
+            
+            '</div>',
+  data: {
+    message: 'hello'
+  }
+})
+
+
+/////////////////////
 var app = new Vue({
   el: '#app',
   data: {
