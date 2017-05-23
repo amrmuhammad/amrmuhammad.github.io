c31af1364979ec8c8f0ab40a987c27d984683758
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-05-23 23:12:15 +0200

    Update main.js

--- math/js/main.js
+++ math/js/main.js
@@ -5,11 +5,20 @@ var Vue = require('./lib/vuejs/v2_2_3/vue.js')
 
 /////////////////////
 Vue.component('matrix', {
-  template: '<div id="" > ' +
-            
+  template: '<div id="matrix1" > ' +
+              '<template v-for="1 to rows">' +
+              '<tr>' +
+                '<template v-for="1 to cols"> +
+                '<td>' +
+                '<input type="text">' +
+                '</td>' +
+              '</tr>' +
+              '</template' +
             '</div>',
   data: {
-    message: 'hello'
+    message: 'hello',
+    rows : 3,
+    cols : 3
   }
 })
 
