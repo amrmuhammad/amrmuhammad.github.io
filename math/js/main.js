1f9f5f83883c4ae1326cc72cd1a627ea4480cb7b
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-06-10 01:50:34 +0200

    Create main.js

--- math/js/main.js
+++ math/js/main.js
@@ -60,7 +60,7 @@ html_code +=  '<input id="bg_input" type="text">'
 //$('#app').append("<matrix></matrix>")
 var bg_value = ""
 $("bg_input").change(function(){
-  
+  alert("background changed")
   var bg_value = $("bg_input").val()
   if(bg_value.length == 6) {
     bg_value = "#" + bg_value
