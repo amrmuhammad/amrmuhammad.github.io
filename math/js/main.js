23b73fcfcf813c8ef1c350088819689911c205e1
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-07-05 01:57:16 +0200

    Update main.js

--- math/js/main.js
+++ math/js/main.js
@@ -63,6 +63,7 @@ html_code +=  '<input id="bg_input" type="text">'
 function add_css_experimenting_section(html_code) {
   html_code += '<div id="css_exp_section">'
   html_code += 'Element id # : <input id="elem_id" type="text">'
+  html_codd += '<br />'
   html_code += 'CSS property name : <select id="css_prop_name">'
   html_code += '  <option value="1">top</option>'
   html_code += '  <option value="2">left</option>'
