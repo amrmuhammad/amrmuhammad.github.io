79d9ba330c08bda76e22f84e577e64762c63ffbc
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-09-13 15:27:09 +0200

    Update multiply_n_by_n.js

--- math/0.2/js/multiply_n_by_n.js
+++ math/0.2/js/multiply_n_by_n.js
@@ -46,9 +46,10 @@ var html_code = ""
 ///////////////////////////////////
 html_code += 'Matrix A size : <input id="mat_A_size" type="number">'
 html_code += '<br />'
-html_code += 'Matrix B size: <input id="mat_B_size" type="number">'
+html_code += 'Matrix B size : <input id="mat_B_size" type="number">'
 html_code += '<br />'
 html_code += '<button id="set_matrices_size_button">Set matrices size</button>'
+html_code += '<br />'
 ///////////////////////////////////
 // Construct the matrix DOM elements
 function construct_matrix(matrix_id, matrix_label)
