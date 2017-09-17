75831590d0be3fb54ac896825dcc78703cf09c65
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-09-17 23:27:06 +0200

    Update main_app.js

--- 0.2/js/main_app.js
+++ 0.2/js/main_app.js
@@ -1,15 +1,16 @@
 
 var $ = require('/math/0.2/js/lib/jquery/v3_2_1/jquery.min.js')
 
-
+var root_path = "https://amrmuhammad.github.io/" 
+    
 $("#site_language").change(function(){
   
   var lang = $("#site_language").val()
   
   if(lang === "ar") {
-    location.assign("/0.2/lang/ar/index.html")
+    location.assign(root_path + "0.2/lang/ar/index.html")
   } else if (lang === "en") {
-    location.assign("/0.2/index.html")
+    location.assign(root_path + "0.2/index.html")
   }
   
   $("body").append("<p>" + lang + "</p>")
