127d52f5668bcdae31de8d1e400802e070b51b6b
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-09-17 21:46:30 +0200

    Update main_app.js

--- 0.2/js/main_app.js
+++ 0.2/js/main_app.js
@@ -1,1 +1,13 @@
 
+
+$("#site_language").change(function(){
+  
+  var lang = $("#site_language").val()
+  
+  if(lang == "العربية") {
+    location.assign("/0.2/lang/ar/index.html")
+  } else if (lang == "English") {
+    location.assign("/0.2/index.html")
+  }
+  
+})
