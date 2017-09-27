513e406982b3b9618a4637876c7dabb94b48013f
amrmuhammad <eng.amrmuhammad@gmail.com>
2017-09-27 22:03:24 +0200

    Update main.js

--- math/0.2/js/lang/ar/main.js
+++ math/0.2/js/lang/ar/main.js
@@ -73,6 +73,8 @@ html_code += construct_matrix("matrix3", "مصفوفة الناتج:")
 html_code += '<br /> <br />'
 html_code += '<h2> مصادر : </h2>'
 html_code += '<a href="https://ar.m.wikipedia.org/wiki/%D9%85%D8%B5%D9%81%D9%88%D9%81%D8%A9_(%D8%B1%D9%8A%D8%A7%D8%B6%D9%8A%D8%A7%D8%AA)"> ويكيبيديا : مصفوفة (رياضيات)</a>'
+html_code += '<br /> <br />'
+html_code += 'Content is available under <a href="#">CC BY-SA 3.0</a>'
 ///////////////////////////////////
 // Add an input textbox to select 
 // background color
