88757a5670f9c5a1211494891a0020e8a36895c7
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-02-09 08:04:13 +0200

    Update main.js

--- githubtools/main.js
+++ githubtools/main.js
@@ -2,7 +2,7 @@
 //var ClientOAuth2 = require('client-oauth2')
 
 
-var GitHub = require('../github-tools/GitHubJS/lib/Github.js');
+var GitHub = require('../github-tools/GitHubJS/lib/GitHub.js');
 var $ = require('/math/0.2/js/lib/jquery/v3_2_1/jquery.min.js')
 
 /*
