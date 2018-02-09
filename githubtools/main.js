f730767dfb991108e727505660b96ebc92cd9f51
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-02-09 20:31:10 +0200

    Update main.js

--- githubtools/main.js
+++ githubtools/main.js
@@ -61,10 +61,10 @@ $("#gh_pat_ok").click(function() {
 
 
 
-var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
-me.listNotifications(function(err, notifications) {
+//var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
+//me.listNotifications(function(err, notifications) {
    // do some stuff
-});
+//});
 
 /*
 var clayreimann = gh.getUser('clayreimann');
@@ -73,7 +73,7 @@ clayreimann.listStarredRepos(function(err, repos) {
 });
 */
 
-var sourceRepo = gh.getRepo('dunso', 'pdf-parser')
-sourceRepo.getContents('master', '', false, function(error, result, response) {
-  $('body').append(result)
-})
+//var sourceRepo = gh.getRepo('dunso', 'pdf-parser')
+//sourceRepo.getContents('master', '', false, function(error, result, response) {
+//  $('body').append(result)
+//})
