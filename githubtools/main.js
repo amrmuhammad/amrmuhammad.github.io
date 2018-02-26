65cd60dbc67e4331bd549a3b81dedd55f0e5790b
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-02-26 21:17:47 +0200

    Update main.js

--- githubtools/main.js
+++ githubtools/main.js
@@ -51,12 +51,16 @@ var model = {
 
 $("#gh_operations_button").click(function() {
   if(model.gh_operations_menu_displayed === false) {
-    var html_code = '<a href="#" class="menu_item">Copy files/folders between repositories</a>'
+    var html_code = '<a href="#" id="gh_ops_copy" class="menu_item">Copy files/folders between repositories</a>'
     $("#gh_operations_div").append(html_code)
   } else {
      model.gh_operations_menu_displayed = false
-     $("#gh_operations_div").remove()
+     $("#gh_operations_div .menu_item").remove()
   }
+})
+
+$("#gh_ops_copy").click(function() {
+   
 })
 
 ////////////////////////////////
