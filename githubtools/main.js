76f0b1bbbdc74062956d98fed86807b8f65a318d
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-04-03 17:07:28 +0300

    Update main.js

--- githubtools/main.js
+++ githubtools/main.js
@@ -62,7 +62,7 @@ var fileUrl = 'https://unpkg.com/github-api@3.0.0/dist/GitHub.bundle.js'
 
 var url = fileUrl
     
-const req = new XMLHttpRequest();
+var req = new XMLHttpRequest();
 req.open('GET', url, true);
 //req.withCredentials = withCredentials || credentials === 'include';
 req.responseType = 'blob';
