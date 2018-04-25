bfb65c5729a904275a289991f7839eed7c563bf2
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-04-25 22:19:55 +0300

    Update util.js

--- githubtools/util.js
+++ githubtools/util.js
@@ -1,9 +1,13 @@
 var $ = require('../math/0.2/js/lib/jquery/v3_2_1/jquery.min.js')
 
+///////////////////////////////////////////////
+var separator = '<br />*******<br />*******<br />'
+
 
 function log(text) {
    $('#debug_div').append(text + separator)
 }
+////////////////////////////////////////////////
 
 log('Beginning of util.js')
 /*********************************************************/
