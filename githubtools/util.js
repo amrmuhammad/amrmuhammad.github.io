f41d10d5b45dd74d3882a4f0e751a83023478346
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-04-26 03:19:56 +0300

    Update util.js

--- githubtools/util.js
+++ githubtools/util.js
@@ -55,7 +55,7 @@ log(JSON.stringify(req.prototype))
 
 
 //applyRequestHeaders(req, headers)
-
+/*
 req.onload = function (oEvent){
   //const ok = req.status >= 200 && req.status < 300;
   //return {body: req.response, status: req.status};
@@ -78,9 +78,9 @@ req.onload = function (oEvent){
   FileSaver.saveAs(blob, "GitHub.bundle.js")
    
 }
+*/
 
 
-/*
 req.addEventListener('progress', function (progressEvent) {
   log('request progress')
 });
@@ -89,7 +89,7 @@ req.addEventListener('error', function (errorEvent) {
 });
 req.addEventListener('abort', function (abortEvent) {
 });
-*/
+
 
 req.send()
 
