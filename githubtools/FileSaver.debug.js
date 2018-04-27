6bd92f589afaab78fb3c7f7766d4c0f74ef56e70
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-04-27 07:25:12 +0300

    Update FileSaver.debug.js

--- githubtools/FileSaver.debug.js
+++ githubtools/FileSaver.debug.js
@@ -38,6 +38,8 @@ log('Beginning of FileSaver.debug.js')
 /*global self */
 /*jslint bitwise: true, indent: 4, laxbreak: true, laxcomma: true, smarttabs: true, plusplus: true */
 
+/*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/src/FileSaver.js */
+
 
 /////////////////////////////////
 
