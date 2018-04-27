b550792c2faa3f833fa221810057846e9072f0e2
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-04-27 03:55:20 +0300

    Update FileSaver.debug.js

--- githubtools/FileSaver.debug.js
+++ githubtools/FileSaver.debug.js
@@ -1,1 +1,15 @@
 
+ 
+var $ = require('../math/0.2/js/lib/jquery/v3_2_1/jquery.min.js') 
+
+
+/////////////////////////////////////////////// 
+var separator = '<br />*******<br />*******<br />' 
+
+function log(text) { 
+  $('#debug_div').append(text + separator)
+} 
+
+//////////////////////////////////////////////// 
+
+log('Beginning of FileSaver.debug.js') */
