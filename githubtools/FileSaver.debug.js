7233b293c4ed9f7ccadc944ba24d4e7c6d27a1d4
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-04-27 07:15:27 +0300

    Update FileSaver.debug.js

--- githubtools/FileSaver.debug.js
+++ githubtools/FileSaver.debug.js
@@ -33,7 +33,22 @@ log('Beginning of FileSaver.debug.js')
  * License: MIT
  *   See https://github.com/eligrey/FileSaver.js/blob/master/LICENSE.md
  */
- /////////////////////////////////
+/////////////////////////////////
+
+/*global self */
+/*jslint bitwise: true, indent: 4, laxbreak: true, laxcomma: true, smarttabs: true, plusplus: true */
+
+/*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/src/FileSaver.js */
+
+export var saveAs = saveAs || (function(view) {
+	"use strict";
+	// IE <10 is explicitly unsupported
+
+}(
+	   typeof self !== "undefined" && self
+	|| typeof window !== "undefined" && window
+	|| this
+));
 
 /////////////////////////////////
 
