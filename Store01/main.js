c707044a027b41c47c84e543972e9434ed8eef09
amrmuhammad <eng.amrmuhammad@gmail.com>
2018-06-14 07:08:57 +0300

    Create main.js

---
+++ Store01/main.js
@@ -0,0 +1,29 @@
+
+
+var $ = require('../math/0.2/js/lib/jquery/v3_2_1/jquery.min.js')
+
+///////////
+
+var separator = '<br />*******<br />*******<br />'
+
+function log(text) {
+   $('#debug_div').append(text + separator)
+}
+
+///////////
+
+
+var html_code = ""
+
+///////////////////////////////////
+html_code += '<hr>'
+html_code += '<a id="sell_your_products_link">Sell your products in our store</a>'
+html_code += '<br /> <br />'
+html_code += '<hr>'
+
+////////////////////////////////////
+$('#app').append(html_code)
+////////////////////////////////////
+$('#app').append('<div id="debug_div"></div>')
+
+////////////////////////////////////
