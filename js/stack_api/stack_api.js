aebe4e57987f3a6ac0607fa9487b56d5baad8d71
amrmuhammad <20337820+amrmuhammad@users.noreply.github.com>
2018-07-05 17:30:52 +0300

    Update stack_api.js

--- js/stack_api/stack_api.js
+++ js/stack_api/stack_api.js
@@ -1,1 +1,40 @@
+// This file is MIT licensed, see amrmuhammad.github.io/LICENSE.md for details
 
+
+// The code for this file is based on Github.js 
+// https://github.com/amrmuhammad/amrmuhammad.github.io/blob/master/github-tools/GitHubJS/lib/GitHub.js
+
+// with the following license:
+
+/**
+ * @file
+ * @copyright  2013 Michael Aufreiter (Development Seed) and 2016 Yahoo Inc.
+ * @license    Licensed under {@link https://spdx.org/licenses/BSD-3-Clause-Clear.html BSD-3-Clause-Clear}.
+ *             Github.js is freely distributable.
+ */
+
+
+/* eslint valid-jsdoc: ["error", {"requireReturnDescription": false}] */
+
+//import Gist from './Gist';
+
+
+
+
+
+/**
+ * SE encapsulates the functionality to create various API wrapper objects.
+ */
+class SE {
+   /**
+    * Create a new SE.
+    * @param {Requestable.auth} [auth] - the credentials to authenticate to SE. If auth is
+    *                                  not provided requests will be made unauthenticated
+    * @param {string} [apiBase=https://api.github.com] - the base Github API URL
+    */
+   constructor(auth, apiBase = 'https://api.github.com') {
+      this.__apiBase = apiBase;
+      this.__auth = auth || {};
+   }
+  
+}
