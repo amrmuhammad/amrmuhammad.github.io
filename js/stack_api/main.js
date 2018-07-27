b280a61c1adddc75af7df1bb2a55cd2dce94d6e8
amrmuhammad <20337820+amrmuhammad@users.noreply.github.com>
2018-07-27 22:22:02 +0300

    Update main.js

--- js/stack_api/main.js
+++ js/stack_api/main.js
@@ -13,9 +13,13 @@ function log(text, debug_div) {
 var $ = require('../../math/0.2/js/lib/jquery/v3_2_1/jquery.min.js')
 
 try {
+  var escapeHtml = require('../component/escape-html/v1.0.3/index.js')
+  
   var stack_api = require('./stack_api.js')
 } catch(err) {
-   log(err + JSON.stringify(err), '#app')
+   
+   var escapedString = escapeHtml(JSON.stringify(err))
+   log(err + escapedString, '#app')
 }
 
 ///////////
