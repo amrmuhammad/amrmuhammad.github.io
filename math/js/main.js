9585050280f591aeebee0913964f5b3623bd04e2
amrmuhammad <20337820+amrmuhammad@users.noreply.github.com>
2021-01-16 03:53:49 +0200

    Update main.js

--- math/js/main.js
+++ math/js/main.js
@@ -96,7 +96,7 @@ function add_css_experimenting_section(html_code) {
   return html_code
   
 }
-//html_code = add_css_experimenting_section(html_code)
+html_code = add_css_experimenting_section(html_code)
 //////////////////////////
 // Attach elements to DOM
 $("#app").append(html_code)
