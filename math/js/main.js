bed0a8191ff7b960a554b3d926e00b5c131a790b
amrmuhammad <20337820+amrmuhammad@users.noreply.github.com>
2021-01-18 02:07:47 +0200

    Update main.js

--- math/js/main.js
+++ math/js/main.js
@@ -19,39 +19,9 @@ function log(text, debug_div) {
 ///////////
 
 ///////////////////
-              
-
-var rows = 3
-var cols = 3
-var html_code = ""
-///////////////////////////////////
-// Construct the matrix DOM elements
-function construct_matrix(matrix_id, matrix_label)
-{
-  var html_code = '<div id="' + matrix_id + '">' 
-  html_code += '<div>' + matrix_label + '</div>'
-  for (i=0; i< rows; i++) {
-   html_code += '<tr>'
-   for (j=0; j< cols; j++) {
-     html_code += '<td>'
-     html_code += '<input type="text" class="matrix_element" id="mat_elem_' + (i+1) + '_' + (j+1) + '">'
-     html_code += '</td>'
-   }
-   html_code += '</tr>'
-  }
-  html_code += '</div>'
-  
-  html_code += '<div id="' + matrix_id + '_error_messages"></div>'
-  return html_code
-}
-
-html_code += construct_matrix("matrix1", "First matrix:")
-html_code += '<br />'
-html_code += construct_matrix("matrix2", "Second matrix:")
-html_code += '<br /> <br  />'
-html_code += '<button id="multiply_matrices_button">Multiply matrices</button>'  
-html_code += '<br /> <br />'
-html_code += construct_matrix("matrix3", "Result matrix:")
+bar view = require('./view.js')             
+var html_code = view.matrices_view
+///////////////////////////////
 ///////////////////////////////////
 // Add an input textbox to select 
 // background color
