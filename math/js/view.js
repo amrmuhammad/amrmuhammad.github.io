ec932c34bb3e896dd646f38686d337a6eddd0b2b
amrmuhammad <20337820+amrmuhammad@users.noreply.github.com>
2021-01-21 01:38:04 +0200

    Update view.js

--- math/js/view.js
+++ math/js/view.js
@@ -42,7 +42,7 @@ function init_css()
   //$("#matrix1").css("border-color", "black")
 
   $("#matrix1").css("border", "1px solid")
-  $("#matrix1").css("margin", "auto")
+  $("#matrix1").css("margin", "0")
 
   //$("#matrix2").css("width", "100%")
  // $("#matrix2").css("padding", "5%")
@@ -53,7 +53,7 @@ function init_css()
   //$("#matrix2").css("border-color", "black")
 
   $("#matrix2").css("border", "1px solid")
-  $("#matrix2").css("margin", "auto")
+  $("#matrix2").css("margin", "0")
 
   //$("#matrix3").css("width", "100%")
   //$("#matrix3").css("padding", "5%") 
@@ -64,7 +64,7 @@ function init_css()
   //$("#matrix3").css("border-color", "black")
 
   $("#matrix3").css("border", "1px solid")
-  $("#matrix3").css("margin", "auto")
+  $("#matrix3").css("margin", "0")
 
   // $("#matrix1").css("position", "relative")
   // $("#matrix2").css("width", "100%")
