6e3b0eaab3e6adcf3930262667bc46f53a8bb291
amrmuhammad <20337820+amrmuhammad@users.noreply.github.com>
2021-01-27 00:59:36 +0200

    Update view.js

--- math/js/view.js
+++ math/js/view.js
@@ -42,7 +42,7 @@ function init_css_matrix(matrix_id) {
   $("#" + matrix_id).css("padding-top", "1%")
 
   $("#" + matrix_id).css("border", "1px solid")
-  $("#" + matrix_id).css("border-radius", "10px")
+  //$("#" + matrix_id).css("border-radius", "10px")
   $("#" + matrix_id).css("margin", "0")
   $("#" + matrix_id).css("background-color", "#1e81b0")
   
@@ -51,7 +51,7 @@ function init_css_matrix(matrix_id) {
   $("#" + matrix_id + " div").css("font-weight", "bold")
   $("#" + matrix_id + " div").css("font-size", "2em")
   
-  $("#" + matrix_id + " input").css("border-radius", "10px")
+  //$("#" + matrix_id + " input").css("border-radius", "10px")
 
 }
 function init_css()
