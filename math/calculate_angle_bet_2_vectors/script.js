30a596e842313128d9334054faab02162df21bab
Amr Muhammad <20337820+amrmuhammad@users.noreply.github.com>
2023-05-24 19:01:50 +0300

    Create script.js

---
+++ math/calculate_angle_bet_2_vectors/script.js
@@ -0,0 +1,8 @@
+const calculate = () => {
+const vector1 = document.getElementById("vector1").value.split(",");
+const vector2 = document.getElementById("vector2").value.split(",");
+const angle = Math.acos((vector1[1] * vector2[1]) + (vector1[0] * vector2[0])) * 180 / Math.PI;
+document.getElementById("angle").innerText = angle;
+}
+
+document.getElementById("calculate").addEventListener("click", calculate);
