08968709dcca2de0906fce8ba5b35781aeb055c1
Amr Muhammad <20337820+amrmuhammad@users.noreply.github.com>
2023-05-24 19:07:50 +0300

    Update script.js

--- math/calculate_angle_bet_2_vectors/script.js
+++ math/calculate_angle_bet_2_vectors/script.js
@@ -2,7 +2,7 @@ const calculate = () => {
 const vector1 = document.getElementById("vector1").value.split(",");
 const vector2 = document.getElementById("vector2").value.split(",");
 const angle = Math.acos((vector1[1] * vector2[1]) + (vector1[0] * vector2[0])) * 180 / Math.PI;
-document.getElementById("angle").innerText = angle + + " degrees";
+document.getElementById("angle").innerText = angle + " degrees";
 }
 
 document.getElementById("calculate").addEventListener("click", calculate);
