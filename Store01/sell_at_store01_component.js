cc9d1dabb8cf4eb70df410680e5704f043134805
Amr Muhammad <20337820+amrmuhammad@users.noreply.github.com>
2025-07-31 19:09:34 +0300

    Update sell_at_store01_component.js

--- Store01/sell_at_store01_component.js
+++ Store01/sell_at_store01_component.js
@@ -1,17 +1,41 @@
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 var html_code = `
 
 <!-- pop-up dialog box, containing a form -->
-<dialog id="sell_dialog">
+<dialog id="sell_dialog_box">
   <form method="dialog">
     <p>
       <label for="full_name">Full Name:</label>
       <input type="text" name="full_name" id="full_name_input" placeholder="Enter your Full Name...">
     </p>
     <div>
-      <button id="cancel" type="reset">Cancel</button>
-      <button type="submit">Submit</button>
+      <button id="cancel_btn" type="reset">Cancel</button>
+      <button id="submit_btn" type="submit">Submit</button>
     </div>
   </form>
 </dialog>
 
 `;
+
+$('#app').append(html_code)
+////////////////////////////////////////////////////////////////////////////////////////////
+const dialog = $("#sell_dialog_box");
+const submitButton = $("#sell_dialog_box > #submit_btn ");
+const cancelButton = $("#sell_dialog_box > #cancel_btn");
+
+////////////////////////////////////////////////////////////////////////////////////////////
+$("#sell_your_products_link").click(/*todo*/) 
+////////////////////////////////////////////////////////////////////////////////////////////
+//dialog.returnValue = "";
+
+// Update button opens a modal dialog
+updateButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// Form cancel button closes the dialog box
+cancelButton.click( () => {
+  dialog.close("");
+});
+////////////////////////////////////////////////////////////////////////////////////////////
