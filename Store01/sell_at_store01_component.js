3bb62b4ac53ad8b666c0f103602c508bca1dcdfd
Amr Muhammad <20337820+amrmuhammad@users.noreply.github.com>
2025-07-31 18:30:11 +0300

    Create sell_at_store01_component.js

---
+++ Store01/sell_at_store01_component.js
@@ -0,0 +1,17 @@
+var html_code = `
+
+<!-- pop-up dialog box, containing a form -->
+<dialog id="sell_dialog">
+  <form method="dialog">
+    <p>
+      <label for="full_name">Full Name:</label>
+      <input type="text" name="full_name" id="full_name_input" placeholder="Enter your Full Name...">
+    </p>
+    <div>
+      <button id="cancel" type="reset">Cancel</button>
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+</dialog>
+
+`;
